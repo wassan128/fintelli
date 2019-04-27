9f4cb35d55fac84e83f2ad75bf1f3745de12b841
wassan128 <wassan.taiko.master@gmail.com>
2019-04-27 22:30:39 +0900

    [add] first tap

---
+++ main.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def init_servo():
+	GPIO.setmode(GPIO.BCM)
+
+	gp_out = 12
+	GPIO.setup(gp_out, GPIO.OUT)
+	servo = GPIO.PWM(gp_out, 50)
+
+	return servo
+
+
+def tap(servo):
+	servo.start(0.0)
+
+	servo.ChangeDutyCycle(10.0)
+	time.sleep(0.1)
+
+	servo.ChangeDutyCycle(8.0)
+	time.sleep(0.5)
+
+	GPIO.cleanup()
+
+
+if __name__ == "__main__":
+	servo = init_servo()
+	tap(servo)
+
